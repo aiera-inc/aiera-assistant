faa97268f777fc24fe5d9108c05accba2fb62664
jacquelinegarrahan <jacqueline.garrahan@aiera.com>
2023-11-26 20:32:05 -0800

    Set up setup configuration

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os import path
+import versioneer
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+setup(
+    name="aiera-gpt",
+    description="Aiera Chat Engine",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(exclude=["bin", "sandbox"]),
+    include_package_data=True,
+    python_requires=">=3.9",
+)
