8b818e4ba47fcc65af365b91a10ee6d95294886a
jacquelinegarrahan <jacqueline.garrahan@aiera.com>
2024-01-02 14:10:28 -0800

    Check in application

--- aiera_assistant/main.py
+++ aiera_assistant/main.py
@@ -27,33 +27,33 @@ def main():
     if 'generated' not in st.session_state:
         st.session_state['generated'] = st.session_state['assistant'].begin_conversation()
 
-    if 'citations' not in st.session_state:
-        st.session_state["citations"] = []
+    #if 'citations' not in st.session_state:
+    #    st.session_state["citations"] = []
 
     if 'model_name' not in st.session_state:
         st.session_state['model_name'] = [ st.session_state['assistant'].model_name]
 
 
     # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
-    clear_button = st.sidebar.button("Clear Conversation", key="clear")
-    st.sidebar.title("Citations")
-    col = st.sidebar.columns(1)[0]
-    for citation in st.session_state["citations"]:
-        col.write(citation)
+    #clear_button = st.sidebar.button("Clear Conversation", key="clear")
+    #st.sidebar.title("Citations")
+    #col = st.sidebar.columns(1)[0]
+    #for citation in st.session_state["citations"]:
+    #    col.write(citation)
 
     # reset everything
-    if clear_button:
-        st.session_state['generated'] = st.session_state['assistant'].begin_conversation()
-        st.session_state['messages'] = []
-        st.session_state['model_name'] = [st.session_state['assistant'].model_name]
+    #if clear_button:
+    #    st.session_state['generated'] = st.session_state['assistant'].begin_conversation()
+    #    st.session_state['messages'] = []
+    #    st.session_state['model_name'] = [st.session_state['assistant'].model_name]
 
-        st.session_state['assistant'].close_chat()
-        del st.session_state['assistant']
-        st.session_state['assistant'] = AieraAssistant(
-            openai_settings = openai_settings,
-            db_settings = db_settings,
-            aiera_settings=aiera_settings
-        )
+    #    st.session_state['assistant'].close_chat()
+    #    del st.session_state['assistant']
+    #    st.session_state['assistant'] = AieraAssistant(
+    #        openai_settings = openai_settings,
+    #        db_settings = db_settings,
+    #        aiera_settings=aiera_settings
+    #    )
 
 
     # container for chat history
@@ -95,13 +95,13 @@ def main():
 
                 else:
                     content = mess["content"]
-                    if mess["annotations"]:
-                        for annotation in mess["annotations"]:
+                    #if mess["annotations"]:
+                    #    for annotation in mess["annotations"]:
                             # check that the text starts with unicode marker
                             # appears to be an error with model generating citations that use some of the text
-                            if annotation["text"][1] == "u" or "†" in annotation["text"]:
-                                content = content.replace(annotation["text"], f" [{len(citations)}]")
-                                citations.append(f"[{len(citations)}] {annotation['quote']}")
+                    #        if annotation["text"][1] == "u" or "†" in annotation["text"]:
+                    #            content = content.replace(annotation["text"], f" [{len(citations)}]")
+                    #            citations.append(f"[{len(citations)}] {annotation['quote']}")
 
                     with st.chat_message('Aiera', avatar=f"{ROOT_DIR}/aiera_assistant/assets/favicon.ico"):
                         st.write(content)
